73c06a04503d0bf355f1f96c0e7c2aa88f9e97e5
Abderrazak <bouadma.abderrazak@gmail.com>
2013-06-14 16:44:01 +0200

    .added upload

--- src/main/java/com/cdc/fast/ws/cxf/CXFDocumentService.java
+++ src/main/java/com/cdc/fast/ws/cxf/CXFDocumentService.java
@@ -5,12 +5,15 @@ import com.cdc.fast.ws.sei.DocumentService;
 import com.cdc.pcp.common.manager.Extension;
 import com.cdc.pcp.common.model.ParapheurNodeInformation;
 import com.cdc.pcp.common.service.PCPExtensionService;
+import com.cdc.pcp.common.service.exception.UploadFileNodesException;
 import com.healthmarketscience.rmiio.RemoteInputStream;
 import com.healthmarketscience.rmiio.RemoteInputStreamClient;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.mail.util.ByteArrayDataSource;
 import javax.xml.namespace.QName;
@@ -18,11 +21,20 @@ import javax.xml.soap.SOAPException;
 import javax.xml.soap.SOAPFactory;
 import javax.xml.soap.SOAPFault;
 import javax.xml.ws.soap.SOAPFaultException;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
 
 @WebService(endpointInterface = "com.cdc.fast.ws.sei.DocumentService", portName = "DocumentPort", targetNamespace = "documentTarget")
 public class CXFDocumentService extends AbstractCommonService implements DocumentService {
+
+    private static final Logger logger = Logger.getLogger(CXFDocumentService.class.getName());
 
     @Autowired
     private PCPExtensionService extensionService;
@@ -42,7 +54,13 @@ public class CXFDocumentService extends AbstractCommonService implements Documen
                 RemoteInputStream remoteInputStream = nodeService.getNodeInputStream(documentId);
                 fileInputStream = RemoteInputStreamClient.wrap(remoteInputStream);
             } catch (IOException e) {
-
+                try {
+                    SOAPFault fault = SOAPFactory.newInstance().createFault();
+                    fault.setFaultString(e.getMessage());
+                    throw new SOAPFaultException(fault);
+                } catch (SOAPException e1) {
+                    throw new RuntimeException("Problem downloading document : " + e1.getMessage());
+                }
             }
 
             // prepare result
@@ -78,5 +96,50 @@ public class CXFDocumentService extends AbstractCommonService implements Documen
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    @Override
+    public String upload(String filename, String label, String comment, String circuit, DataHandler content) {
+
+        // create temp file
+        File tempFile = null;
+        try {
+            tempFile = File.createTempFile("parapheur-" + new Date(), ".upload");
+            content.writeTo(new FileOutputStream(tempFile));
+        } catch (IOException e) {
+            SOAPFault fault = null;
+            try {
+                fault = SOAPFactory.newInstance().createFault();
+                fault.setFaultString("Erreur lors de la creqtion d'un fichier temporaire");
+                throw new SOAPFaultException(fault);
+            } catch (SOAPException e1) {
+                throw new RuntimeException(e1);
+            }
+        }
+
+        // upload into the repository
+        List<String> createdNodeRefs = null;
+        String noderefId = null;
+        try {
+            createdNodeRefs = nodeService.uploadFileNodes(Arrays.asList(new File[]{tempFile}), buildListOfOneElement(filename), buildListOfOneElement(label), buildListOfOneElement(comment), userService.getUserInformation(getUsername()), circuit);
+            noderefId = ((createdNodeRefs.size() > 0) ? createdNodeRefs.get(0) : "");
+            logger.info("Node created : " + noderefId);
+        } catch (UploadFileNodesException e) {
+            SOAPFault fault = null;
+            try {
+                fault = SOAPFactory.newInstance().createFault();
+                fault.setFaultString(e.getMessage());
+                throw new SOAPFaultException(fault);
+            } catch (SOAPException e1) {
+                throw new RuntimeException(e1);
+            }
+        }
+
+        // return the nodeRefId of the created document
+        return noderefId;
+    }
+
+    private List<String> buildListOfOneElement(String item) {
+        return Arrays.asList(new String[]{item});
     }
 }
--- src/main/java/com/cdc/fast/ws/sei/DocumentService.java
+++ src/main/java/com/cdc/fast/ws/sei/DocumentService.java
@@ -1,15 +1,28 @@
 package com.cdc.fast.ws.sei;
 
+import javax.activation.DataHandler;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
+
+/**
+ * <p>Main Service Document business scope</p>
+ */
 @WebService
 public interface DocumentService {
 
     @WebMethod(operationName = "download")
-        //TODO documentId should be mandatory
+
+    /**
+     * downloads the document specified by its ID
+     */
     DocumentContentVO download(@WebParam(name = "documentId", mode = WebParam.Mode.IN) String documentId);
 
+    @WebMethod(operationName = "upload")
+    /**
+     * uploads a document into Parapheur repository and returns the generated documentId
+     */
+    String upload(@WebParam(name = "filename", mode = WebParam.Mode.IN) String filename, @WebParam(name = "label", mode = WebParam.Mode.IN) String label, @WebParam(name = "comment", mode = WebParam.Mode.IN) String comment, @WebParam(name = "circuit", mode = WebParam.Mode.IN) String circuit, @WebParam(name = "content", mode = WebParam.Mode.IN) DataHandler content);
 
 }
